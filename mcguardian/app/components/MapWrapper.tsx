f02a47800cbcf4754bae9a2b1e97d52df5dd99b3
nil-akkurt <nil.akkurt@mail.mcgill.ca>
2025-01-18 17:59:45 -0500

    this took sm longer than it should've

--- mcguardian/app/components/MapWrapper.tsx
+++ mcguardian/app/components/MapWrapper.tsx
@@ -10,27 +10,7 @@ interface MapWrapperProps {
 const DynamicMap = dynamic(() => import('./map'), { ssr: false });
 
 const MapWrapper: React.FC<MapWrapperProps> = ({ accessToken }) => {
-  return(
-    <div style={{ position: 'relative', width: '100%', height: '100%'}}>
-    <DynamicMap accessToken={accessToken} />
-    <div 
-    style={{
-      position: 'absolute',
-      top: '20px',
-      left: '20px',
-      zIndex: 10,
-      padding: '5px',
-      borderRadius: '50%',
-    }}
-    >
-      <img 
-        src="/logo.png"
-        alt="Logo"
-        style={{width: '500px', height: '100px', objectFit: 'contain' }}
-        />
-        </div>
-        </div>
-  ) 
+  return <DynamicMap accessToken={accessToken} />
 };
 
 export default MapWrapper;
--- mcguardian/app/page.tsx
+++ mcguardian/app/page.tsx
@@ -2,13 +2,33 @@
 import React from 'react';
 import MapWrapper from './components/MapWrapper';
 
+const Logo: React.FC = () => (
+  <div 
+    style={{
+      position: "absolute",
+      top: "20px",
+      left: "20px",
+      zIndex: 10,
+    }}
+    >
+      <img 
+        src="/logo.png"
+        style={{
+          width: "50px",
+          height: "50px",
+          objectFit: "contain",
+        }}
+        />
+    </div>
+);
+
 export default function Home() {
   const accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || '';
 
   return (
     <div>
-      <h1>Maybe tis works</h1>
       <MapWrapper accessToken={accessToken} />
+      <Logo />
     </div>
   );
 }
