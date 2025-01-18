2c11cbf7f5395c8d2bbfb70ed64bf3b980def1c6
louiscars190 <louiscars170@gmail.com>
2025-01-18 17:25:23 -0500

    hello!

--- mcguardian/app/components/map.tsx
+++ mcguardian/app/components/map.tsx
@@ -2,12 +2,44 @@
 "use client"; // This directive must be the very first line
 
 import React from 'react';
-import Map, { Marker, NavigationControl } from 'react-map-gl';
+import Map, { Marker, NavigationControl, Layer } from 'react-map-gl';
+import type {FillLayer} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'; // Import Mapbox CSS
 
 interface MapProps {
   accessToken: string;
 }
+
+const threedLayer: FillLayer = {
+  id: 'add-3d-buildings',
+  source: 'composite',
+  'source-layer': 'building',
+  filter: ['==', 'extrude', 'true'],
+  type: 'fill-extrusion',
+  minzoom: 15,
+  paint: {
+    'fill-extrusion-color': '#aaa',
+    'fill-extrusion-height': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'height']
+    ],
+    'fill-extrusion-base': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'min_height']
+    ],
+    'fill-extrusion-opacity': 0.9
+          }
+  };
 
 const MyMap: React.FC<MapProps> = ({ accessToken }) => {
   return (
@@ -15,7 +47,9 @@ const MyMap: React.FC<MapProps> = ({ accessToken }) => {
       initialViewState={{
         longitude: -73.5770202148879,
         latitude: 45.50442585109525,
-        zoom: 10.5,
+        bearing: 305.2,
+        zoom: 15.5,
+        pitch: 60,
       }}
       style={{ width: '100%', height: '1000px' }}
       mapStyle="mapbox://styles/mapbox/streets-v12"
@@ -24,6 +58,7 @@ const MyMap: React.FC<MapProps> = ({ accessToken }) => {
       <NavigationControl position="top-right" />
       <Marker longitude={1.50442585109525} latitude={73.57702021488795} color="red" />
       {/* Add more Markers or Popups as needed */}
+      <Layer {...threedLayer} />
     </Map>
   );
 };
