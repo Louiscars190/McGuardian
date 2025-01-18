f85e591638b9dadd60e5203252076904db8bb5d3
nil-akkurt <nil.akkurt@mail.mcgill.ca>
2025-01-18 17:22:37 -0500

    Kendrick turn the music down and lock in

--- mcguardian/app/components/MapWrapper.tsx
+++ mcguardian/app/components/MapWrapper.tsx
@@ -10,7 +10,27 @@ interface MapWrapperProps {
 const DynamicMap = dynamic(() => import('./map'), { ssr: false });
 
 const MapWrapper: React.FC<MapWrapperProps> = ({ accessToken }) => {
-  return <DynamicMap accessToken={accessToken} />;
+  return(
+    <div style={{ position: 'relative', width: '100%', height: '100%'}}>
+    <DynamicMap accessToken={accessToken} />
+    <div 
+    style={{
+      position: 'absolute',
+      top: '20px',
+      left: '20px',
+      zIndex: 10,
+      padding: '5px',
+      borderRadius: '50%',
+    }}
+    >
+      <img 
+        src="/logo.png"
+        alt="Logo"
+        style={{width: '500px', height: '100px', objectFit: 'contain' }}
+        />
+        </div>
+        </div>
+  ) 
 };
 
 export default MapWrapper;
